e41c424ce9a25d09286d12d560a82e7cba818ff7
ayalamiguelsan <85299964+ayalamiguelsan@users.noreply.github.com>
2023-08-02 23:30:07 +0000

    .

--- main.py
+++ main.py
@@ -1,4 +1,49 @@
 #primer archivo
+
+#Ejercicio 1
+'''
 print("hola mundo")
+'''
+#Ejercicio 2
+'''
 citizen=  'hola mundo'
 print(citizen)
+'''
+#Ejercico 3
+''''
+nombre=input('introduce tu nombre:')
+print+("soy"+nombre+"!")
+'''
+#Ejercicio 4
+'''
+print(((3+2)/(2*5))**2)
+'''
+#Ejercicio 5
+'''
+horas= float(input("cuantas horas trabajas: "))
+pago= float(input("cuanto te pagan por hora: "))
+print (pago*horas)
+'''
+#ejercicio 6
+'''
+n=int(input("numero entero "))
+suma=(n*(n+1)/2)
+print(suma)
+'''
+#Ejercicio 7
+'''
+p=input("peso corporal ")
+e=input("estatura ")
+imc=round(float(p)/float(e)**2,2)
+print(imc)
+'''
+#Ejercicio 8
+'''
+n=int(input("numero entero "))
+m=int(input("numero entero "))
+c=(n/m)
+print(c)
+r=(n%m)
+print(r)
+'''
+#Ejercicio 9
